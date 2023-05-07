d71f5fa172c42621b768f33f0732569a1997b6d1
Susnata Biswas <108311263+alphaCulf-404@users.noreply.github.com>
2023-05-07 16:43:19 +0530

    Update Quick Exponention.cpp

--- Mathematics/Quick Exponention.cpp
+++ Mathematics/Quick Exponention.cpp
@@ -5,25 +5,50 @@
 
 using namespace std;
 
-#define ar array
-#define ll long long
+template <typename A, typename B>
+ostream &operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template <typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type>
+ostream &operator<<(ostream &os, const T_container &v)
+{
+    os << '{';
+    string sep;
+    for (const T &x : v)
+        os << sep << x, sep = ", ";
+    return os << '}';
+}
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T)
+{
+    cerr << ' ' << H;
+    dbg_out(T...);
+}
+#ifdef LOCAL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+#define ld long double
+#define sza(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+#define int long long int
+#define pb push_back
+#define no cout << "NO" \
+                << "\n"
+#define yes cout << "YES" \
+                 << "\n"
+#define sp(a) setprecision(a)
 
 const int MAX_N = 1e5 + 5;
-const ll MOD = 1e9 + 7;
-const ll INF = 1e9;
+const int MOD = 1e9 + 7;
+const int INF = 1e9;
+const ld EPS = 1e-9;
 
-// Recursive
-ll qexp(ll a, ll b, ll m) {
-    if (b == 0) return 1;
-    ll res = qexp(a, b / 2, m);
-    res = res * res % m;
-    if (b % 2) res = res * a % m;
-    return res;
-}
-
-// Iterative (slightly faster)
-ll qexp(ll a, ll b, ll m) {
-    ll res = 1;
+int qexp(int a, int b, int m) {
+    int res = 1;
     while (b) {
         if (b % 2) res = res * a % m;
         a = a * a % m;
@@ -32,18 +57,21 @@ ll qexp(ll a, ll b, ll m) {
     return res;
 }
 
-void solve() {
+void solve()
+{
     int a, b, m; cin >> a >> b >> m;
     cout << qexp(a, b, m) << "\n";
 }
 
-int main() {
+signed main()
+{
     ios_base::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
-    int tc = 1;
-    // cin >> tc;
-    for (int t = 1; t <= tc; t++) {
-        // cout << "Case #" << t << ": ";
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin >> t;
+    while(t--) {
+        // cout << "Case #" << t+1 << ": ";
         solve();
     }
 }
