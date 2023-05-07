71eb79415b8e0fa8ff94b0194a3fcfc53fd89e81
Susnata Biswas <108311263+alphaCulf-404@users.noreply.github.com>
2023-05-08 00:15:31 +0530

    Add files via upload

---
+++ Mathematics/eww.cpp
@@ -0,0 +1,95 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template <typename A, typename B>
+ostream &operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template <typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type>
+ostream &operator<<(ostream &os, const T_container &v)
+{
+    os << '{';
+    string sep;
+    for (const T &x : v)
+        os << sep << x, sep = ", ";
+    return os << '}';
+}
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T)
+{
+    cerr << ' ' << H;
+    dbg_out(T...);
+}
+#ifdef LOCAL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+#define ld long double
+#define sza(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+typedef vector<int> vi;
+typedef vector<vector<int> > vvi;
+// #define int long long int
+#define pb push_back
+#define no cout << "NO" \
+                << "\n"
+#define yes cout << "YES" \
+                 << "\n"
+#define sp(a) setprecision(a)
+#define ar array
+
+const int N = 1e5 + 5;
+const int MOD = 1e9 + 7;
+const int INF = 1e9;
+const ld EPS = 1e-9;
+
+int gcd(int a, int b, int& x, int& y) 
+{
+    if (b == 0) {
+        x = 1;
+        y = 0;
+        return a;
+    }
+    int x1, y1;
+    int d = gcd(b, a % b, x1, y1);
+    x = y1;
+    y = x1 - y1 * (a / b);
+    return d;
+}
+
+bool any_solution(int a, int b, int c, int &x, int &y)
+{
+    int x0, y0;
+    int g = gcd(a, b, x0, y0);
+    if(c%g) return false;
+
+    x = x0* (c/g);
+    y = y0* (c/g);
+
+    if(a < 0) x = -x;
+    if(b < 0) y = -y;
+    return true;
+}
+
+void solve()
+{
+    int a, b, c, x, y; cin >> a >> b >> c;
+    if(!any_solution(a, b, c, x, y)) cout << "NO SOLUTION" << "\n";
+    else cout << x << " " << y << "\n";
+}
+ 
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        // cout << "Case #" << t+1 << ": ";
+        solve();
+    }
+}
