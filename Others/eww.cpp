de372232fa25609cf9e31130f6654996f86f50d6
Susnata Biswas <108311263+alphaCulf-404@users.noreply.github.com>
2023-05-08 02:38:21 +0530

    Add files via upload

---
+++ Others/eww.cpp
@@ -0,0 +1,100 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template <typename A, typename B>
+ostream &operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template <typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type>
+ostream &operator<<(ostream &os, const T_container &v)
+{
+    os << '{';
+    string sep;
+    for (const T &x : v)
+        os << sep << x, sep = ", ";
+    return os << '}';
+}
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T)
+{
+    cerr << ' ' << H;
+    dbg_out(T...);
+}
+#ifdef LOCAL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+#define ld long double
+#define sza(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+#define int long long int
+#define pb push_back
+#define no cout << "NO" \
+                << "\n"
+#define yes cout << "YES" \
+                 << "\n"
+#define sp(a) setprecision(a)
+#define ar array
+
+const int N = 1e5 + 5;
+const int MOD = 1e9 + 7;
+const int INF = 1e9;
+const ld EPS = 1e-9;
+
+int binarySearch(vector<int>arr, int l, int r, int x)
+{
+    while (l <= r)
+    {
+        int m = l + (r - l) / 2;
+
+        if (arr[m] == x)
+        {
+            return m;
+        }
+ 
+        if (arr[m] < x)
+        {
+            l = m + 1;
+        }
+        else
+        {
+            r = m - 1;
+        }
+    }
+    return -1;
+}
+
+void solve()
+{
+    int n, x; cin >> n >> x; 
+    vector<int>arr;
+
+    for(int i = 0; i < n; i++)
+    {
+        int x; cin >> x; arr.pb(x);
+    }
+    sort(arr.begin(), arr.end());
+    int ans = binarySearch(arr, 0, n-1, x);
+
+    if(ans != -1) cout << "DAMN!! you find it" << "\n";
+    else cout << "The Element is not present in this array!!" << "\n";
+}
+
+
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        // cout << "Case #" << t+1 << ": ";
+        solve();
+    }
+}
