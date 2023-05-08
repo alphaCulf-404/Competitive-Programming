50a06d0a5b157f5fe7f08a106ce75dea85b8d421
Susnata Biswas <108311263+alphaCulf-404@users.noreply.github.com>
2023-05-09 03:21:39 +0530

    Update Digit DP.cpp

--- Dynamic Programming/Digit DP.cpp
+++ Dynamic Programming/Digit DP.cpp
@@ -1,66 +1,124 @@
-// Find the sum of the digits of the numbers between a and b (0 <= a <= b <= 1e9)
-// Problem link: https://www.spoj.com/problems/CPCRC1C/
+// Find the number between L and R whose sum of the digits of that numnber % D = K (0 <= a <= b <= 1e9)
+//Atcoder Education Dp contest
 
 #include <bits/stdc++.h>
 
 using namespace std;
 
-#define ar array
-#define ll long long
+template <typename A, typename B>
+ostream &operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template <typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type>
+ostream &operator<<(ostream &os, const T_container &v)
+{
+    os << '{';
+    string sep;
+    for (const T &x : v)
+        os << sep << x, sep = ", ";
+    return os << '}';
+}
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T)
+{
+    cerr << ' ' << H;
+    dbg_out(T...);
+}
+#ifdef LOCAL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
 
-const int MAX_N = 1e5 + 1;
+#define ld long double
+#define sza(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+typedef vector<int> vi;
+#include <unordered_set>
+typedef vector<vector<int>> vvi;
+#define int long long int
+#define pb push_back
+#define no cout << "NO" \
+                << "\n"
+#define yes cout << "YES" \
+                 << "\n"
+#define sp(a) setprecision(a)
+
 const int MOD = 1e9 + 7;
-const int INF = 1e9;
-const ll LINF = 1e18;
 
-vector<int> num;
-ll dp[10][9 * 10][2];
+int len;
+int D;
+string L, R;
 
-// dp[pos][sum][flag]
-// pos = current position, starting from the left (0-index)
-// sum = sum of all digits till the given position
-// flag = the number we are building has already become smaller than b? [0 = no, 1 = yes]
+int dp[10010][2][2][100];
 
-ll memo(int pos, int sum, int flag) {
-    if (pos == num.size()) return sum;
-    if (dp[pos][sum][flag] != -1) return dp[pos][sum][flag];
-
-    ll res = 0;
-    int lmt = (flag) ? 9 : num[pos];
-    for (int i = 0; i <= lmt; i++) {
-        int next_flag = (i < lmt) ? 1 : flag;
-        res += memo(pos + 1, sum + i, next_flag);
+int rec(int level, int tlo, int thi, int sumD)
+{
+    if (level == len)
+    {
+        if (sumD == 0)
+            return 1;
+        else
+            return 0;
     }
-    return dp[pos][sum][flag] = res;
+    if (dp[level][tlo][thi][sumD] != -1)
+    {
+        return dp[level][tlo][thi][sumD];
+    }
+    int ans = 0;
+    int lo = 0;
+    if (tlo == 1)
+    {
+        lo = L[level] - '0';
+    }
+    int hi = 9;
+    if (thi == 1)
+    {
+        hi = R[level] - '0';
+    }
+    for (int i = lo; i <= hi; i++)
+    {
+        int ntlo = tlo;
+        if (i != L[level] - '0')
+            ntlo = 0;
+
+        int nthi = thi;
+        if (i != R[level] - '0')
+            nthi = 0;
+
+        ans += rec(level + 1, ntlo, nthi, (sumD + i) % D);
+        ans %= MOD;
+    }
+    return dp[level][tlo][thi][sumD] = ans;
 }
 
-ll calc(int n) {
-    num.clear();
-    while (n) {
-        num.push_back(n % 10);
-        n /= 10;
+void solve()
+{
+    cin>>L>>R;
+    cin>>D;
+    int cnt = R.length()-L.length();
+    string temp = "";
+    while(cnt--){
+        temp+='0';
     }
-    reverse(num.begin(), num.end());
-    memset(dp, -1, sizeof dp);
-    return memo(0, 0, 0);
-}
+    L = temp+L;
+    len = R.length();
 
-void solve() {
-    while (true) {
-        int a, b; cin >> a >> b;
-        if (a == -1 && b == -1) return;
-        cout << calc(b) - calc(a - 1) << "\n";
-    }
-}
+    memset(dp,-1,sizeof(dp));
+    cout<<rec(0,1,1,0)<<endl;
+} 
 
-int main() {
+signed main()
+{
     ios_base::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
+    cin.tie(0);
+    cout.tie(0);
     // freopen("input.txt", "r", stdin);
     // freopen("output.txt", "w", stdout);
 
-    int tc; tc = 1;
-    for (int t = 1; t <= tc; t++) {
+    int tc;
+    cin >> tc;
+    for (int t = 1; t <= tc; t++)
+    {
         // cout << "Case #" << t  << ": ";
         solve();
     }
