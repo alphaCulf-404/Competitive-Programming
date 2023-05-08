df0a286bd029153dfad5afde0508c14ba1174d62
Susnata Biswas <108311263+alphaCulf-404@users.noreply.github.com>
2023-05-08 16:43:02 +0530

    Update Closest Pairs.cpp

--- Geometry/Sweep Line/Closest Pairs.cpp
+++ Geometry/Sweep Line/Closest Pairs.cpp
@@ -66,13 +66,16 @@ void solve() {
     }
 }
 
-int main() {
+signed main()
+{
     ios_base::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
-    int tc = 1;
-    // cin >> tc;
-    for (int t = 1; t <= tc; t++) {
-        // cout << "Case #" << t << ": ";
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        // cout << "Case #" << t+1 << ": ";
         solve();
     }
 }
