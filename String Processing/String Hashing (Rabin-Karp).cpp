55a68e157b1f1792ea5b53a1d697b47453e1e7b2
Susnata Biswas <108311263+alphaCulf-404@users.noreply.github.com>
2023-05-08 14:05:03 +0530

    Update String Hashing (Rabin-Karp).cpp

--- String Processing/String Hashing (Rabin-Karp).cpp
+++ String Processing/String Hashing (Rabin-Karp).cpp
@@ -14,20 +14,56 @@
 
 using namespace std;
 
-#define ar array
-#define ll long long
+template <typename A, typename B>
+ostream &operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template <typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type>
+ostream &operator<<(ostream &os, const T_container &v)
+{
+    os << '{';
+    string sep;
+    for (const T &x : v)
+        os << sep << x, sep = ", ";
+    return os << '}';
+}
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T)
+{
+    cerr << ' ' << H;
+    dbg_out(T...);
+}
+#ifdef LOCAL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
 
-const int MAX_N = 1e5 + 5;
-const ll MOD = 1e9 + 7;
-const ll INF = 1e9;
+#define ld long double
+#define sza(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+#define int long long int
+#define pb push_back
+#define no cout << "NO" \
+                << "\n"
+#define yes cout << "YES" \
+                 << "\n"
+#define sp(a) setprecision(a)
+#define ar array
+
+const int N = 1e5 + 5;
+const int MOD = 1e9 + 7;
+const int INF = 1e9;
+const ld EPS = 1e-9;
 
 mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
-ll rand(ll l, ll r) {return uniform_int_distribution(l, r)(rng);}
+int rand(int l, int r) {return uniform_int_distribution(l, r)(rng);}
 const int BASE = rand(300, 1e9);
 const int NUM_MOD = 2;
-const ll MODS[] = {(int)1e9 + 2277, (int)1e9 + 5277};
+const int MODS[] = {(int)1e9 + 2277, (int)1e9 + 5277};
 struct Hash {
-    vector<ll> H[NUM_MOD], P[NUM_MOD];
+    vector<int> H[NUM_MOD], P[NUM_MOD];
     Hash(string s) {
         for (int i = 0; i < NUM_MOD; i++) {
             P[i].push_back(1);
@@ -40,8 +76,8 @@ struct Hash {
             }
         }
     } 
-    ar<ll,NUM_MOD> operator()(int l, int r) {
-        ar<ll,NUM_MOD> res;
+    ar<int,NUM_MOD> operator()(int l, int r) {
+        ar<int,NUM_MOD> res;
         for (int i = 0; i < NUM_MOD; i++) {
             res[i] = (H[i][r + 1] - H[i][l] * P[i][r + 1 - l]) % MODS[i];
             if (res[i] < 0) res[i] += MODS[i];
@@ -50,7 +86,8 @@ struct Hash {
     }
 };
 
-int cnt_occ(string s, string t) {
+int cnt_occ(string s, string t) 
+{
     int n = s.size(), m = t.size();
     Hash hs(s), ht(t);
     int res = 0;
@@ -60,18 +97,22 @@ int cnt_occ(string s, string t) {
     return res;
 }
 
-void solve() {
+void solve() 
+{
     string s, t; cin >> s >> t;
     cout << cnt_occ(s, t) << "\n";
 }
 
-int main() {
+signed main()
+{
     ios_base::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
-    int tc = 1;
-    // cin >> tc;
-    for (int t = 1; t <= tc; t++) {
-        // cout << "Case #" << t << ": ";
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        // cout << "Case #" << t+1 << ": ";
         solve();
     }
 }
