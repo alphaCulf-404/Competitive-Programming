282880b6dbe9bdda0a59e3fbf4991b719c2218a6
Susnata Biswas <108311263+alphaCulf-404@users.noreply.github.com>
2023-05-09 02:09:09 +0530

    Update Longest Increasing Subsequence (LIS).cpp

--- Dynamic Programming/Longest Increasing Subsequence (LIS).cpp
+++ Dynamic Programming/Longest Increasing Subsequence (LIS).cpp
@@ -1,42 +1,90 @@
-// Find the longest increasing subsequence (LIS) in the array of length n
-// Time complexity: O(nlogn)
-// Problem link: https://cses.fi/problemset/task/1145
-
 #include <bits/stdc++.h>
 
 using namespace std;
 
-#define ar array
-#define ll long long
+template <typename A, typename B>
+ostream &operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template <typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type>
+ostream &operator<<(ostream &os, const T_container &v)
+{
+    os << '{';
+    string sep;
+    for (const T &x : v)
+        os << sep << x, sep = ", ";
+    return os << '}';
+}
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T)
+{
+    cerr << ' ' << H;
+    dbg_out(T...);
+}
+#ifdef LOCAL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
 
-const int MAX_N = 1e5 + 1;
+#define ld long double
+#define sza(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+#define int long long int
+#define pb push_back
+#define no cout << "NO" \
+                << "\n"
+#define yes cout << "YES" \
+                 << "\n"
+#define sp(a) setprecision(a)
+#define ar array
+
+const int N = 1e5 + 5;
 const int MOD = 1e9 + 7;
 const int INF = 1e9;
-const ll LINF = 1e18;
+const ld EPS = 1e-9;
+const int MAX_N = 1000;
+const int MAX_W = 1000;
 
+int dp[MAX_N];
+vector<int>s;
+int n;
 
-
-void solve() {
-    int n; cin >> n;
-    vector<int> dp;
-    for (int i = 0; i < n; i++) {
-        int x; cin >> x;
-        auto it = lower_bound(dp.begin(), dp.end(), x);
-        if (it == dp.end()) dp.push_back(x);
-        else *it = x;
+int lis(int i) 
+{
+    if (i == n) return 0;
+    if (dp[i] != -1) return dp[i];
+    int ans = 1;
+    for (int j = i+1; j < n; j++)
+    {
+        if (s[j] > s[i]) ans = max(ans, lis(j) + 1);
     }
-    cout << dp.size() << "\n";
+    return dp[i] = ans;
+}
+ 
+
+void solve()
+{
+    memset(dp, -1, sizeof(dp));
+    cin >> n;
+    for(int i = 0; i < n; i++)
+    {
+        int x; cin >> x; s.pb(x);
+    }
+    cout << lis(0) << "\n";
 }
 
-int main() {
+signed main()
+{
     ios_base::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
-    // freopen("input.txt", "r", stdin);
-    // freopen("output.txt", "w", stdout);
-
-    int tc; cin >> tc;
-    for (int t = 1; t <= tc; t++) {
-        // cout << "Case #" << t  << ": ";
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        // cout << "Case #" << t+1 << ": ";
         solve();
     }
 }
