910eee3666008d3ec5d67158d0d1dbafea1647ec
Susnata Biswas <108311263+alphaCulf-404@users.noreply.github.com>
2023-05-08 13:11:50 +0530

    Update KMP (Prefix function).cpp

--- String Processing/KMP (Prefix function).cpp
+++ String Processing/KMP (Prefix function).cpp
@@ -1,51 +1,96 @@
-// Given a string s (with length n) and a pattern p (with length m), find all occurrence of p in s
+// Given a string s (with length n) and a pattern t (with length m), find all occurrence of p in s
 // Time complexity: O(n + m)
 // Problem link: https://cses.fi/problemset/task/1753/
 
 #include <bits/stdc++.h>
- 
+
 using namespace std;
- 
-#define ar array
-#define ll long long
- 
-const int MAX_N = 1e5 + 5;
-const ll MOD = 1e9 + 7;
-const ll INF = 1e9;
- 
-// f[i] = length of the longest proper prefix of the substring s[0...i] which is also a suffix of this substring
-vector<int> prefix_func(string s) {
-    int n = s.size();
-    vector<int> f(n); 
-    for (int i = 1; i < n; i++) {
-        int j = f[i - 1];
-        while (j && s[i] != s[j]) j = f[j - 1];
-        f[i] = j + (s[i] == s[j]);
-    }
-    return f;
+
+template <typename A, typename B>
+ostream &operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template <typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type>
+ostream &operator<<(ostream &os, const T_container &v)
+{
+    os << '{';
+    string sep;
+    for (const T &x : v)
+        os << sep << x, sep = ", ";
+    return os << '}';
 }
- 
-int cnt_occ(string s, string t) {
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T)
+{
+    cerr << ' ' << H;
+    dbg_out(T...);
+}
+#ifdef LOCAL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+#define ld long double
+#define sza(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+#define int long long int
+#define pb push_back
+#define no cout << "NO" \
+                << "\n"
+#define yes cout << "YES" \
+                 << "\n"
+#define sp(a) setprecision(a)
+#define ar array
+
+const int N = 1e5 + 5;
+const int MOD = 1e9 + 7;
+const int INF = 1e9;
+const ld EPS = 1e-9;
+
+vector<int> KMP(string &s)
+{
+    int n = s.length(); vector<int>kmp(n+1, 0);
+    
+    int i = 0, j = -1; kmp[0] = -1;
+    
+    while(i < n)
+    {
+        while(j != -1 && s[i] != s[j]) j = kmp[j];
+        j++; i++;
+        kmp[i] = j;
+    }
+    return kmp;
+}
+
+int cnt_occ(string s, string t) 
+{
     string ts = t + "#" + s;
     int n = t.size(), m = s.size(), nm = ts.size();
-    auto f = prefix_func(ts);
+    vector<int>f = KMP(ts);
     int res = 0;
     for (int i = n + 1; i < nm; i++) res += (f[i] == n);
     return res;
 }
- 
-void solve() {
+
+void solve()
+{
     string s, t; cin >> s >> t;
-    cout << cnt_occ(s, t) << "\n";
+    cout << cnt_occ(s,t) << "\n";
 }
- 
-int main() {
+
+
+signed main()
+{
     ios_base::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
-    int tc = 1;
-    // cin >> tc;
-    for (int t = 1; t <= tc; t++) {
-        // cout << "Case #" << t << ": ";
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        // cout << "Case #" << t+1 << ": ";
         solve();
     }
 }
