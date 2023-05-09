15c9ede41af601145d33a849f5b31f3070b394ef
Susnata Biswas <108311263+alphaCulf-404@users.noreply.github.com>
2023-05-09 15:57:23 +0530

    Update BFS.cpp

--- Graphs/Graph Traversal/BFS.cpp
+++ Graphs/Graph Traversal/BFS.cpp
@@ -2,12 +2,41 @@
 
 using namespace std;
 
-#define ar array
-#define ll long long
+template <typename A, typename B>
+ostream &operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template <typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type>
+ostream &operator<<(ostream &os, const T_container &v)
+{
+    os << '{';
+    string sep;
+    for (const T &x : v)
+        os << sep << x, sep = ", ";
+    return os << '}';
+}
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T)
+{
+    cerr << ' ' << H;
+    dbg_out(T...);
+}
+#ifdef LOCAL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
 
-const int MAX_N = 1e5 + 5;
-const ll MOD = 1e9 + 7;
-const ll INF = 1e9;
+#define ld long double
+#define sza(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+typedef vector<int> vi;
+#include <unordered_set>
+typedef vector<vector<int>> vvi;
+#define int long long int
+#define pb push_back
+#define no cout << "NO" << "\n"
+#define yes cout << "YES" << "\n"
+#define sp(a) setprecision(a)
 
 int n, m;
 vector<int> adj[MAX_N], dist;
@@ -37,15 +66,22 @@ void solve() {
     bfs(1);
 }
 
-int main() {
+signed main()
+{
     ios_base::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
+    cin.tie(0);
+    cout.tie(0);
     // freopen("input.txt", "r", stdin);
     // freopen("output.txt", "w", stdout);
 
-    int tc; tc = 1;
-    for (int t = 1; t <= tc; t++) {
+    int tc;
+    cin >> tc;
+
+    for (int i = 1; i <= tc; i++)
+    {
         // cout << "Case #" << t  << ": ";
         solve();
     }
+    return 0;
+}}
 }
