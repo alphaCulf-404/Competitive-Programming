291b8e5d60014b711d79469c8ebcfd4ff13d6874
Susnata Biswas <108311263+alphaCulf-404@users.noreply.github.com>
2023-09-13 17:06:39 +0530

    Update Segment Tree (Recursive).cpp

--- Data Structures/Segment Tree/Segment Tree (Recursive).cpp
+++ Data Structures/Segment Tree/Segment Tree (Recursive).cpp
@@ -1,76 +1,156 @@
-// Segment tree for range minimum query, point set update
-// Problem link: https://cses.fi/problemset/task/1649
+// Range Minimum & Point Update
+// Dynamic Range Minimum Queries: https://cses.fi/problemset/task/1649
 
 #include <bits/stdc++.h>
-
+// #include<ext/pb_ds/assoc_container.hpp>
+// #include<ext/pb_ds/tree_policy.hpp>
+ 
 using namespace std;
-
-#define ar array
-#define ll long long
-
-const int MAX_N = 2e5 + 1;
-const int MOD = 1e9 + 7;
-const int INF = 1e9;
-const ll LINF = 1e18;
-
-int n, q, arr[MAX_N], st[4 * MAX_N];
+// using namespace __gnu_pbds;
  
-void build(int node, int start, int end) {
-    if (start == end) {
-        st[node] = arr[start];
+typedef long long int ll;
+typedef long double   ld;
+typedef unsigned long long ull;
+#define endl '\n'
+#define int         ll
+#define double      ld
+#define ff          first
+#define ss          second
+#define sz(x)       (int)x.size()
+#define pb          push_back
+#define ppb         pop_back
+#define mp          make_pair
+#define all(x)      x.begin(), x.end()
+#define ret(x)      {cout<<x; return;}
+#define vi          vector<int>
+#define vpii        vector<pair<int,int>>
+#define pii         pair<int,int>
+#define mi          map<int,int>
+#define rep(i,j,n)  for(int i=j;i<n;i++)
+#define inf         9223372036854775807
+#define cinv(v,n)   rep(i,0,n)cin>>v[i];
+#define pr(x)       cout<<x<<endl;
+#define PI          3.14159265358979323846264338327950288419716939937510
+#define dbg(x)      cout<<#x<<": "<<x<<endl;
+#define dbgg(x,y)   cout<<#x<<": "<<x<<" "<<#y<<": "<<y<<endl;
+#define fl(x)        cerr<<"Flag :"<<x<<endl;
+#define MOD         1000000007
+// #define MAXN        200001
+#define no cout << "NO\n"
+#define yes cout << "YES\n"
+#define Code ios_base::sync_with_stdio(false);
+#define By cin.tie(NULL);
+#define Susnata cout.tie(NULL);
+ 
+//template<class T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update> ; // find_by_order(iterator), order_of_key
+ 
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr<< #x <<": "; _print(x); cerr<<endl;
+#else
+#define debug(x)
+#endif
+ 
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(ld t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+void _print(bool t) {cerr << t;}
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+//template <class T> void _print(ordered_set <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+//template <class T> void _print(ordered_set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+ 
+vector<ll> sieve(int n) {int*arr = new int[n + 1](); vector<ll> vect; for (int i = 2; i <= n; i++)if (arr[i] == 0) {vect.push_back(i); for (int j = 2 * i; j <= n; j += i)arr[j] = 1;} return vect;}
+ll mod_add(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a + b) % m) + m) % m;}
+ll mod_mul(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a * b) % m) + m) % m;}
+ll mod_sub(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a - b) % m) + m) % m;}
+
+int a[200005], seg[4*200005];
+
+void build(int ind, int low, int high)
+{
+    if(low == high){
+        seg[ind] = a[low];
         return;
     }
-    int mid = (start + end) / 2;
-    build(2 * node, start, mid);
-    build(2 * node + 1, mid + 1, end);
-    st[node] = min(st[2 * node], st[2 * node + 1]);
+    int mid = (low + high)/2;
+    build(2*ind+1, low, mid);
+    build(2*ind+2, mid+1, high);
+    seg[ind] = min(seg[2*ind+1], seg[2*ind+2]);
 }
- 
-void update(int node, int start, int end, int idx, int val) {
-    if (start == end) {
-        arr[idx] = val;
-        st[node] = val;
+
+void update(int ind, int pos, int val, int low, int high)
+{
+    if(low == high){
+        seg[ind] = val;
         return;
     }
-    int mid = (start + end) / 2;
-    if (idx <= mid) update(2 * node, start, mid, idx, val);
-    else update(2 * node + 1, mid + 1, end, idx, val);
-    st[node] = min(st[2 * node], st[2 * node + 1]);
+    else
+    {
+        int mid = (low + high)/2;
+        if(pos <= mid) update(2*ind+1, pos, val, low, mid);
+        else update(2*ind+2, pos, val, mid+1, high);
+        seg[ind] = min(seg[2*ind+1], seg[2*ind+2]); 
+    }
 }
 
-int query(int node, int start, int end, int l, int r) {
-    if (start > r || end < l) return INF;
-    if (l <= start && end <= r) return st[node];
-    int mid = (start + end) / 2;
-    return min(query(2 * node, start, mid, l, r), query(2 * node + 1, mid + 1, end, l, r)); 
+int query(int ind, int low, int high, int l, int r)
+{
+    if(low >= l and high <= r) return seg[ind];
+    if(high < l || low > r) return INT_MAX;
+    int mid = (low + high)/2;
+    int left = query(2*ind+1, low, mid, l, r);
+    int right = query(2*ind+2, mid+1, high, l, r);
+    return min(left,right);
 }
- 
-void solve() {
-    cin >> n >> q;
-    for (int i = 1; i <= n; i++) cin >> arr[i];
-    build(1, 1, n);
-    while (q--) {
-        int t; cin >> t;
-        if (t == 1) {
-            int k, u; cin >> k >> u;
-            update(1, 1, n, k, u);
-        }
-        else {
-            int l, r; cin >> l >> r;
-            cout << query(1, 1, n, l, r) << "\n";
+
+void solve()
+{
+    int n, q; cin >> n >> q;
+    for(int i = 0; i < n; i++){
+        cin >> a[i];
+    }
+    build(0, 0, n-1);
+    while(q--) 
+    {
+        int query_type;
+        cin >> query_type;
+        if (query_type == 1) 
+        {
+            int k; int u;
+            cin >> k >> u;
+            k--; 
+            update(0, k, u, 0, n - 1);
+        } 
+        else if (query_type == 2) 
+        {
+             int l, r; cin >> l >> r;
+             l--; r--;
+             cout << query(0, 0, n-1, l, r) << "\n";
         }
     }
 }
 
-int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
-    // freopen("input.txt", "r", stdin);
-    // freopen("output.txt", "w", stdout);
-
-    int tc; tc = 1;
-    for (int t = 1; t <= tc; t++) {
-        // cout << "Case #" << t  << ": ";
+signed main()
+{
+    Code By Susnata
+#ifndef ONLINE_JUDGE
+    freopen("inputf.in", "r", stdin);
+    freopen("outputf.in", "w", stdout);
+#endif
+    int t=1;
+    while (t--)
+    {
         solve();
     }
+    return 0;
 }
